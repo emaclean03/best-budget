8caea9af88e7e648c8a3a46e6f92c3670e7b2e49
Eric <>
2023-01-09 15:39:01 -0500

    Adding starting income transactoin for new accounts

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreAccountRequest;
 use App\Http\Requests\UpdateAccountRequest;
 use App\Models\Account;
+use App\Models\Category;
+use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Cknow\Money\Money;
@@ -34,41 +36,56 @@ class AccountController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreAccountRequest  $request
+     * @param \App\Http\Requests\StoreAccountRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreAccountRequest $request)
     {
         $account = Account::make([
-            'budget_id'=>Auth::user()->budget->id,
-            'account_name'=>$request->accountName,
-            'account_type'=>$request->accountType,
-            'working_balance'=>Money::USD($request->startingBalance),
+            'budget_id' => Auth::user()->budget->id,
+            'account_name' => $request->accountName,
+            'account_type' => $request->accountType,
             'currency' => 'USD',
         ]);
 
         Auth::user()->account()->save($account);
+
+
+        //Add new income to our working balance
+        $account->update([
+            'working_balance'=> Money::USD($account->working_balance)->add(Money::USD($request->startingBalance)),
+        ]);
+
+        //Create the income transaction
+        $amountToAssignCategory = Category::where('category_name', '=', 'Assign Income')->where('user_id', Auth::user()->id)->first();
+         Transaction::create([
+            'user_id'=>Auth::user()->id,
+            'budget_id' => Auth::user()->budget->id,
+            'account_id' => $account->id,
+            'category_id' => $amountToAssignCategory->id,
+            'inflow' => Money::USD($request->startingBalance)->getAmount(),
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Account  $account
+     * @param \App\Models\Account $account
      * @return \Inertia\Response
      */
     public function show(Account $account)
     {
         return Inertia::render('Account/Account', [
             'account' => $account,
-            'transactions'=>$account->transaction()->with('category')->get(),
-            'categories'=>Auth::user()->category()->get(),
+            'transactions' => $account->transaction()->with('category')->get(),
+            'categories' => Auth::user()->category()->get(),
         ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Account  $account
+     * @param \App\Models\Account $account
      * @return \Illuminate\Http\Response
      */
     public function edit(Account $account)
@@ -79,8 +96,8 @@ class AccountController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateAccountRequest  $request
-     * @param  \App\Models\Account  $account
+     * @param \App\Http\Requests\UpdateAccountRequest $request
+     * @param \App\Models\Account $account
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateAccountRequest $request, Account $account)
@@ -91,7 +108,7 @@ class AccountController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Account  $account
+     * @param \App\Models\Account $account
      * @return \Illuminate\Http\Response
      */
     public function destroy(Account $account)
